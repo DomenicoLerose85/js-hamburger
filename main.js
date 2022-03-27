ccef025d2030f4a1e0fcc7d515e2e1a6d5805567
DomenicoLerose85 <domenicolerose85@libero.it>
2022-03-27 18:39:52 +0200

    continue_of_excercise

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
-element.addEventListener("click", myFunction); 
+const hamburgerMenu = document.querySelector ('hamburger-menu');
 
-function myFunction() {
+const hamburgerMenuList = document.querySelector ('hamburger-menu ul li a');
 
+const closeBtn = document.querySelector ('close');
+
+console.log = (hamburgerMenu, hamburgerMenuList, closeBtn);
+
+function myFunction("click") {
+    const hamburgerMenu = document.querySelector ('hamburger-menu') 
+    const hamburgerMenuList = document.querySelector ('hamburger-menu ul li a');
+    const closeBtn = document.querySelector ('close');
+    console.log = (hamburgerMenu, hamburgerMenuList, closeBtn); 
 }
+
+element.addEventListener("click", myFunction); 
